003d34e5b7946268d9608362f2ebb4fec9b05e74
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2022-12-13 11:11:29 -0800

    revised contact page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
             <div className="container">
                 <div className="top">
                     <div className="logo-footer">
-                    <img src={logo} width="160px" height="100px" alt="Palm Springs Rehab Logo" loading="lazy"/>
+                    <img src={logo} width="175px" height="80px" alt="Palm Springs Rehab Logo" loading="lazy"/>
                     </div>
                     <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
                         <BsFillArrowUpCircleFill className='icon' />
