be486ad22a5bef82863b11d4da557f130952e7d6
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-17 10:51:41 -0800

    styling

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { BsFillPinMapFill } from 'react-icons/bs';
-// import { AiOutlinePhone } from 'react-icons/ai';
-// import { AiOutlineMail } from 'react-icons/ai';
+import { AiOutlinePhone } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai';
 
 
 import './ContactForm.css';
@@ -28,37 +28,54 @@ const ContactForm = () => {
 
     return (
         <>
-        <div className="contactForm-mainContainer">
-            <div className='contactForm'>
-                <div className="map">
-                    <div className="location">
-                        <h1>GET IN TOUCH</h1><br />
-                        {/* <p><AiOutlinePhone /><a href="tel:3234521224"> +1 (323) 452-1224  </a></p><br />  */}
-                        {/* <p><AiOutlineMail /> info@domainName.com  </p><br /> */}
-                        <p><BsFillPinMapFill /> 7235 Santa Monica Blvd West Hollywood CA 90046 </p><br />
-                        <p className="admission">Please call our admission department or fill out the form if you have any questions.</p>
-                    </div>
 
-                    <div className="iframe">
-                        <iframe title="map" width="700" height="250" id="gmap_canvas" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=7235%20Santa%20Monica%20Blvd%20West%20Hollywood%20CA%2090046+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">distance maps</a></iframe>
-                    </div>
-                </div>
-
-                <div className="container contact-container">
-                    <form ref={form} onSubmit={sendEmail}>
-                        <h1>SEND A MESSAGE</h1>
-                        <input className="user-input" type="text" name='name' placeholder='Name' required />
-                        <input className="user-input" type="text" name='email' placeholder='Email' required />
-                        <input className="user-input" type="tel" name='phone' placeholder='Phone' required />
-                        <input className="user-input" type="text" name='subject' placeholder='Subject' required />
-                        <textarea className="user-input email-input" name="message" rows="4" placeholder="How can we help?" required ></textarea>
-                        <div className="submit-btn">
-                            <button type='submit' className="btn-modal btn btn-primary">Send</button>
-                        </div>
-                    </form>
-                </div>
+<section className="container">
+<div>
+      <div class="footer_get_touch_inner">
+          <div class="get_form_inner">
+            <div class="get_form_inner_text">
+              <h3>Get In Touch</h3>
+              <p><i>We'll keep your contact information strictly confidential.</i></p><br/>
             </div>
+            <form ref={form} onSubmit={sendEmail}>
+              <div class="inputs">
+                <input type="text" name='name' placeholder='Your Name' required /> <br/>
+                <input type="text" name='email' placeholder='Your Email' required /> <br/>
+                <input type="tel" name='phone' placeholder='Your Phone' required /> <br/>
+                <input type="text" name='subject' placeholder='Subject' required /> <br/>
+
+              </div>
+              <div>
+                <textarea name="message" placeholder="How can we help?" cols="30" rows="10" required></textarea>
+                <input type="submit" value="Submit" />
+              </div>
+            </form>
     </div>
+</div>
+</div>
+</section>
+
+
+<div className="contact-cards-container">
+    
+    <div class="get_form_inner2">
+            <h2>CALL US</h2>
+            <p><AiOutlinePhone /><a href="tel:3238018511"> +1 (323) 8018511</a></p>
+    </div>
+
+    <div class="get_form_inner2">
+            <h2>EMAIL</h2>
+            <p><AiOutlineMail /> info@domainName.com  </p><br />
+    </div>
+
+    <div class="get_form_inner2">
+            <h2>LOCATION</h2>
+            <p><BsFillPinMapFill />7235 Santa Monica Blvd West Hollywood CA 90046</p>
+    </div>
+
+</div>
+    
+
         </>
     )
 }
